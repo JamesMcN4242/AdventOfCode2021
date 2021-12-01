15e6e58ca514c79b02ea122bd4e7d8e7af7a90df
DESKTOP-97TO8KI\James <jamesmcneil42@hotmail.co.uk>
2021-12-01 18:39:22 +0000

    Day one part 2 solution

--- Day1/main.py
+++ Day1/main.py
@@ -13,6 +13,20 @@ def solve_part_one(puzzle_input):
     return increasing
 
 
+def solve_part_two(puzzle_input):
+    increasing = 0
+    window_one = sum([puzzle_input[i] for i in range(0, 2)])
+    for i in range(3, len(puzzle_input)):
+        window_two = window_one - puzzle_input[i-3] + puzzle_input[i]
+        if window_two > window_one:
+            increasing += 1
+    return increasing
+
+
 if __name__ == '__main__':
-    p1_solution = solve_part_one(get_input())
+    int_input = get_input()
+    p1_solution = solve_part_one(int_input)
     print('Part One Solution: ' + str(p1_solution))
+
+    p2_solution = solve_part_two(int_input)
+    print('Part Two Solution: ' + str(p2_solution))
