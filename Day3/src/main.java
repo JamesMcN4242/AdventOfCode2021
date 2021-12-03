e40df689def2ec9d4b0243df4b99f4be53842883
DESKTOP-97TO8KI\James <jamesmcneil42@hotmail.co.uk>
2021-12-03 19:40:57 +0000

    Day 3 part two

--- Day3/src/main.java
+++ Day3/src/main.java
@@ -1,35 +1,24 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 class Day3 {
     public static void main(String[] args) throws IOException {
-        List<String> input = GetInput();
+        List<String> input = Files.readAllLines(Path.of("input.txt"));
         System.out.printf("Part One Answer: %d%n", SolvePartOne(input));
-    }
-
-    private static List<String> GetInput() throws IOException {
-        return Files.readAllLines(Path.of("input.txt"));
+        System.out.printf("Part Two Answer: %d%n", SolvePartTwo(input));
     }
 
     private static int SolvePartOne(List<String> input)
     {
-        int charCount = input.get(0).length();
-        int[] timesOnesAppeared = new int[charCount];
-
-        for (String line : input) {
-            for (int j = 0; j < charCount; ++j) {
-                timesOnesAppeared[j] += line.charAt(j) - 48;
-            }
-        }
-
         int gamma = 0, epsilon = 0;
-        int halfLineCount = input.size() / 2;
+        int[] mostCommonBits = GetMostCommonBits(input);
 
-        for (int i = 0; i < charCount; ++i) {
-            int addition = 1 << (charCount - i - 1);
-            if(timesOnesAppeared[i] > halfLineCount) {
+        for (int i = 0; i < mostCommonBits.length; ++i) {
+            int addition = 1 << (mostCommonBits.length - i - 1);
+            if(mostCommonBits[i] == 1) {
                 gamma += addition;
             } else {
                 epsilon += addition;
@@ -37,5 +26,51 @@ class Day3 {
         }
         
         return gamma * epsilon;
+    }
+
+    private static int SolvePartTwo(List<String> input)
+    {
+        List<String> oxygenRating = input;
+        List<String> co2ScrubRating = new ArrayList<>(input);
+
+        LoopTilCompletion(oxygenRating, false);
+        LoopTilCompletion(co2ScrubRating, true);
+        return Integer.parseInt(oxygenRating.get(0), 2) * Integer.parseInt(co2ScrubRating.get(0), 2);
+    }
+
+    private static void LoopTilCompletion(List<String> input, boolean removeLeastCommon) {
+        int charCount = input.get(0).length();
+        char removeIfOne = removeLeastCommon ? '1' : '0';
+        char removeIfZero = removeLeastCommon ? '0' : '1';
+
+        for(int i = 0; i < charCount && input.size() > 1; ++i)
+        {
+            int mostCommon = GetMostCommonBit(input, i);
+            char toRemove = mostCommon == 1 ? removeIfOne : removeIfZero;
+            int finalI = i;
+            input.removeIf(n -> n.charAt(finalI) == toRemove);
+        }
+    }
+
+    private static int[] GetMostCommonBits(List<String> input) {
+        int charCount = input.get(0).length();
+        int[] mostCommon = new int[charCount];
+
+        for(int i = 0; i < charCount; ++i) {
+            mostCommon[i] = GetMostCommonBit(input, i);
+        }
+        return mostCommon;
+    }
+
+    private static int GetMostCommonBit(List<String> input, int charIndex) {
+        int timesOnesAppeared = 0;
+
+        for (String line : input) {
+            timesOnesAppeared += line.charAt(charIndex) - 48;
+        }
+
+        int halfLineCount = (input.size() / 2) + (input.size() % 2);
+        timesOnesAppeared = timesOnesAppeared >= halfLineCount ? 1 : 0;
+        return timesOnesAppeared;
     }
 }
