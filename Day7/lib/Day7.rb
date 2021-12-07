5325a78bc93f78fbdbd6f882a6a574ab9d9b3815
DESKTOP-97TO8KI\James <jamesmcneil42@hotmail.co.uk>
2021-12-07 13:20:12 +0000

    Day 7 part 2

--- Day7/lib/Day7.rb
+++ Day7/lib/Day7.rb
@@ -7,8 +7,7 @@ module Day7
     file_data
   end
 
-  def self.solve_part_one
-    input_data = load_file.sort
+  def self.solve_part_one(input_data)
     median = input_data[input_data.length / 2]
     moved = 0
     input_data.each { |input|
@@ -17,5 +16,16 @@ module Day7
     moved
   end
 
-  puts solve_part_one
+  def self.solve_part_two(input_data)
+    average = (input_data.sum.to_f / input_data.length.to_f).floor
+    fuel_used = 0
+    input_data.each { |input|
+      fuel_used += ((input - average).abs * ((input - average).abs + 1) / 2)
+    }
+    fuel_used
+  end
+
+  input_data = load_file.sort
+  puts "Part One: %d" %solve_part_one(input_data)
+  puts "Part Two: %d" %solve_part_two(input_data)
 end
