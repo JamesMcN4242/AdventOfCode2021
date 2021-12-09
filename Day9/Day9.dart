70d7d8935b3e28eef73963491ffba81f70bd09d2
DESKTOP-97TO8KI\James <jamesmcneil42@hotmail.co.uk>
2021-12-09 22:51:05 +0000

    Day 9 part 2

--- Day9/Day9.dart
+++ Day9/Day9.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'dart:io';
 
 List<List<int>> GetInput() {
@@ -30,7 +31,49 @@ int SolvePartOne(List<List<int>> board) {
   return value;
 }
 
+int SolvePartTwo(List<List<int>> board) {
+  List<int> basinSizes = List.empty(growable: true);
+
+  for(var row = 0; row < board.length; row++) {
+    for (var column = 0; column < board[row].length; column++) {
+      var currentVal = board[row][column];
+      if ((row == 0 || currentVal < board[row-1][column]) &&
+          (row == board.length - 1 || currentVal < board[row+1][column]) &&
+          (column == 0 || currentVal < board[row][column-1]) &&
+          (column == board[row].length - 1 || currentVal < board[row][column+1])) {
+
+        var currentVal = board[row][column];
+        Queue<List<int>> toConsider = Queue.from([[row-1, column, currentVal], [row+1, column, currentVal], [row, column -1, currentVal], [row, column + 1, currentVal]]);
+        Set<int> inBasin = Set.identity();
+        inBasin.add((row << 16) | column);
+        while(toConsider.length > 0) {
+          var curr = toConsider.removeFirst();
+          if(inBasin.contains((curr[0] << 16) | curr[1]) || !IsPartOfBasin(board, curr[2], curr)) continue;
+
+          var value = board[curr[0]][curr[1]];
+          toConsider.add([curr[0] - 1, curr[1], value]);
+          toConsider.add([curr[0] + 1, curr[1], value]);
+          toConsider.add([curr[0], curr[1] - 1, value]);
+          toConsider.add([curr[0], curr[1] + 1, value]);
+          inBasin.add((curr[0] << 16) | curr[1]);
+        }
+
+        basinSizes.add(inBasin.length);
+      }
+    }
+  }
+
+  basinSizes.sort();
+  var len = basinSizes.length;
+  return basinSizes[len - 1] * basinSizes[len - 2] * basinSizes[len - 3];
+}
+
+bool IsPartOfBasin(List<List<int>> board, int previousValue, List<int> coord) {
+  return coord[0] >= 0 && coord[1] >= 0 && board.length > coord[0] && board[coord[0]].length > coord[1] && board[coord[0]][coord[1]] > previousValue && board[coord[0]][coord[1]] != 9;
+}
+
 void main () {
   var board = GetInput();
   print('Part One: ${SolvePartOne(board)}');
+  print('Part Two: ${SolvePartTwo(board)}');
 }
