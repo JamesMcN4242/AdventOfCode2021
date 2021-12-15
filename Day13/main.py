530adb617b6e85e80abd3b01784196f16ed959c8
James McNeil <jamesmcneil42@hotmail.co.uk>
2021-12-15 12:43:12 +0000

    Day 13 part two

--- Day13/main.py
+++ Day13/main.py
@@ -37,20 +37,34 @@ def solve_part_one(board, folds):
     return sum(x.count('#') for x in board)
 
 
-def fold(board, fold):
-    if fold[0] == 'x':
-        folded = [i[fold[1] + 1:] for i in board]
-        board = [i[0:fold[1]] for i in board]
+def solve_part_two(board, folds):
+    for fold_instruction in folds:
+        board = fold(board, fold_instruction)
+
+    #Purely to make it slightly easier to read
+    for y in range(0, len(board)):
+        for x in range(0, len(board[0])):
+            if board[y][x] == '.':
+                board[y][x] = ' '
+
+    for line in board:
+        print(line)
+
+
+def fold(board, fold_instruction):
+    if fold_instruction[0] == 'x':
+        folded = [i[fold_instruction[1] + 1:] for i in board]
+        board = [i[0:fold_instruction[1]] for i in board]
         for y in range(0, len(board)):
-            for x in range(fold[1] - len(folded[0]), len(board[0])):
-                if folded[y][fold[1] - 1 - x] == '#':
+            for x in range(fold_instruction[1] - len(folded[0]), len(board[0])):
+                if folded[y][fold_instruction[1] - 1 - x] == '#':
                     board[y][x] = '#'
     else:
-        folded = board[fold[1]+1:]
-        board = board[0:fold[1]]
-        for y in range(fold[1] - len(folded), len(board)):
+        folded = board[fold_instruction[1] + 1:]
+        board = board[0:fold_instruction[1]]
+        for y in range(fold_instruction[1] - len(folded), len(board)):
             for x in range(0, len(board[0])):
-                if folded[fold[1] - 1 - y][x] == '#':
+                if folded[fold_instruction[1] - 1 - y][x] == '#':
                     board[y][x] = '#'
 
     return board
@@ -58,3 +72,5 @@ def fold(board, fold):
 
 inputs = setup_board(get_input())
 print('Part One: ' + str(solve_part_one(inputs[0], inputs[1])))
+print('Part Two:')
+solve_part_two(inputs[0], inputs[1])
